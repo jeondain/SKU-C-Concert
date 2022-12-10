e970227bf4deadc2592313d69a862851b0a19f97
jeondain <120189161+jeondain@users.noreply.github.com>
2022-12-10 13:53:55 +0900

    Create 10-5.c

---
+++ Chapter 10 문자열/10-5.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char str[100];
+
+	printf("텍스트를 입력한다 : ");
+	gets_s(str, 99);
+	printf("최종 텍스트 : ");
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U' &&
+			str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u')
+			printf("%c", str[i]);
+	}
+}
